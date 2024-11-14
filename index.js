3c8772cb8eb51645058f3befe0aa0acaa86ffdfe
hw0923309 <hw0923309@otc.edu>
2024-11-13 22:08:57 -0600

    IT WORKS!!!

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// NPM installs the library, then it needs to be imported to be available to the program
+// Neat!
+const axios = require('axios');
+
 function getQuote(){
     axios.get('https://zenquotes.io/api/random')
     .then(response => {
         
         console.log("Here's a motivational quote for you:");
-        console.log(response.data[q]);
+        console.log(response.data[0].q);
+        console.log("-" + response.data[0].a)
     })
     .catch (ex => {
-        console.log("Could not fetch a quote. Try again later.");
+        console.log("Could not fetch a quote. Try again later." + ex);
     });
         
 };
